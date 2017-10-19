535c9b35b539328d9db4ef36a7ac5112df9c0bc4
Martin Ferianc <ferianc.martin@gmail.com>
2017-10-19 12:24:35 +0100

    [Assignment 1E] Adding covariance calculation

---
+++ src/assignment_1/covariance.py
@@ -0,0 +1,13 @@
+import math
+import pandas as pd
+import numpy as np
+
+
+def main(logfile):
+    data = pd.read_csv(logfile, sep=" ", header=None)
+    data = np.vstack((data[0],data[1]))
+    print(np.cov(data))
+
+
+if __name__ == '__main__':
+    main("coordinates.txt")
