ad3f9680eb0a39b908c532dd721bb77a1f21c671
Mike Chen <mikecyj25@gmail.com>
2017-10-19 13:02:16 +0100

    WIP generate calibration figures in batch

--- assignment_1/visualize_calibration.py
+++ assignment_1/visualize_calibration.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import matplotlib.patches as mpatches
 
 
 def main(logfile):
@@ -8,24 +9,44 @@ def main(logfile):
     data = pd.read_csv(logfile_path, sep="\t", header=None)
     fig = plt.figure(1)
 
+    # set figure title
+    plt.suptitle(logfile)
+
     ax1 = plt.subplot(211)
-    line1 = plt.plot(data[0],data[2], label="Actual angle")
-    line2 = plt.plot(data[0],data[1], label ="Reference angle")
+    ax1_line1 = plt.plot(data[0],data[2], "g", label="Actual angle")
+    ax1_line2 = plt.plot(data[0],data[1], "b", label ="Reference angle")
     ax1.set_title('Left motor', fontsize=12)
-    plt.xlabel('Time [ms]', fontsize=12)
+    plt.xlabel('Time [s]', fontsize=12)
     plt.ylabel('Angle [Radians]', fontsize=12)
+
+    # create legends
+    green_patch = mpatches.Patch(color='green', label='Actual angle')
+    blue_patch = mpatches.Patch(color='blue', label='Reference angle')
+
+    # place legends on the bottom right
+    plt.legend(handles=[green_patch, blue_patch], loc=4)
 
     ax2 = plt.subplot(212)
-    line1 = plt.plot(data[0],data[4], label="Actual angle")
-    line2 = plt.plot(data[0],data[3], label ="Reference angle")
+    ax2_line1 = plt.plot(data[0],data[4], "g", label="Actual angle")
+    ax2_line2 = plt.plot(data[0],data[3], "b", label ="Reference angle")
     ax2.set_title('Right motor', fontsize=12)
-    plt.xlabel('Time [ms]', fontsize=12)
+    plt.xlabel('Time [s]', fontsize=12)
     plt.ylabel('Angle [Radians]', fontsize=12)
 
-    plt.show()
+    # place legends on the bottom right
+    plt.legend(handles=[green_patch, blue_patch], loc=4)
+
+    # prevent overlapping
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+    # save as png
+    graph_path = './graphs/' + logfile + '.png'
+    plt.savefig(graph_path, dpi=140)
+
+    # plt.show()
 
 
 if __name__ == '__main__':
     #main("motor_position_1_60.0.log")
     #main("motor_position_1_180.0.log")
-    main("motor_position_4_180.log")
+    main("motor_position_1_60.0.log")
