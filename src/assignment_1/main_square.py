8d594405cc0e26455df62b09ad587fae70b19cbb
Martin Ferianc <ferianc.martin@gmail.com>
2017-10-19 12:33:00 +0100

    Add the main program for 40 cm square

---
+++ src/assignment_1/main_square.py
@@ -0,0 +1,15 @@
+import threading
+import time
+from robot import Robot
+import brickpi
+
+#Initialize the interface
+interface=brickpi.Interface()
+interface.initialize()
+
+Robot = Robot(interface)
+for i in range(4):
+    Robot.travel_straight(40)
+    Robot.rotate_left(90)
+
+interface.terminate()
