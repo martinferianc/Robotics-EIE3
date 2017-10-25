4b09edf1c0dde3ebb377ffc55926326b1309c5a6
George Punter <gsp14@point06.doc.ic.ac.uk>
2017-10-25 10:50:46 +0100

    Adding code for rotate top motor

--- src/robot.py
+++ src/robot.py
@@ -5,18 +5,23 @@ import math
 
 class Robot:
 	def __init__(self, interface, config_file="config.json", touch_ports = None, ultrasonic_port = None):
-		#Motor initialization
-		self.motors = [0,1]
-		self.motorParams = {}
+		# Robot initilization
 		self.interface = interface
 		self.left_speed = 0
 		self.right_speed = 0
 		self.wheel_diameter = 5.3 #cm
 		self.circumference = self.wheel_diameter * math.pi
+		self.pose = 0
+		self.ultra_pose = 0
+
+		#Motor initialization
+		self.motors = [0,1,2]
+		self.motorParams = {}
 
 		#Enabling the motors
 		self.interface.motorEnable(self.motors[0])
 		self.interface.motorEnable(self.motors[1])
+		self.interface.motorEnable(self.motors[2])
 
 		#Open the config file
 		data = None
@@ -27,6 +32,7 @@ class Robot:
 		# Configure motor calibration constants
 		self.distance_calibration = data.get("distance_calibration", 3.05)
 		self.angle_calibration = data.get("angle_calibration", 0.13)
+		self.ultra_angle_calibration = data.get("ultra_angle_calibration", 0.15)
 
 		#Configuring the left motor
 		self.motorParams["left"] = self.interface.MotorAngleControllerParameters()
@@ -52,8 +58,21 @@ class Robot:
 		self.motorParams["right"].pidParameters.k_i = data["right"]["k_i"]
 		self.motorParams["right"].pidParameters.k_d = data["right"]["k_d"]
 
+		#Configure the top motor
+		self.motorParams["top"] = self.interface.MotorAngleControllerParameters()
+		self.motorParams["top"].maxRotationAcceleration = data["top"]["maxRotationAcceleration"]
+		self.motorParams["top"].maxRotationSpeed = data["top"]["maxRotationSpeed"]
+		self.motorParams["top"].feedForwardGain = data["top"]["feedForwardGain"]
+		self.motorParams["top"].minPWM = data["top"]["minPWM"]
+		self.motorParams["top"].pidParameters.minOutput = data["top"]["minOutput"]
+		self.motorParams["top"].pidParameters.maxOutput = data["top"]["maxOutput"]
+		self.motorParams["top"].pidParameters.k_p = data["top"]["k_p"]
+		self.motorParams["top"].pidParameters.k_i = data["top"]["k_i"]
+		self.motorParams["top"].pidParameters.k_d = data["top"]["k_d"]
+
 		self.interface.setMotorAngleControllerParameters(self.motors[0],self.motorParams["left"])
 		self.interface.setMotorAngleControllerParameters(self.motors[1],self.motorParams["right"])
+		self.interface.setMotorAngleControllerParameters(self.motors[2],self.motorParams["top"])
 
 		self.interface.setMotorRotationSpeedReferences(self.motors,[self.left_speed,self.right_speed])
 
@@ -86,35 +105,38 @@ class Robot:
 
 	# Move specified wheel a certain distance
 	def move_wheels(self, distances=[1,1], wheels=[0,1]):
+		print("Distance to move wheels: {}".format(distances))
 
-		if self.ultrasonic_port is not None:
-			print("Distance to move wheels: {}".format(distances))
+		# Retrieve start angle of motors
+		motorAngles_start = self.interface.getMotorAngles(wheels)
+		print("Start Angles: {}".format(motorAngles_start))
 
-			# Retrieve start angle of motors
-			motorAngles_start = self.interface.getMotorAngles(wheels)
-			print("Start Angles: {}".format(motorAngles_start))
+		# Set the reference angles to reach
+		circular_distances = [round((2*x*self.distance_calibration)/self.circumference,2) for x in distances]
+		print("Distance in radians: {}".format(circular_distances))
+		# Angles to end at
+		motorAngles_end = []
+		motorAngles_end.append(round(motorAngles_start[0][0] + circular_distances[0],2))
+		motorAngles_end.append(round(motorAngles_start[1][0] + circular_distances[1],2))
+		print("Angles to end at: {}".format(motorAngles_end))
 
-			# Set the reference angles to reach
-			circular_distances = [round((2*x*self.distance_calibration)/self.circumference,2) for x in distances]
-			print("Distance in radians: {}".format(circular_distances))
-			motorAngles_end = []
+		self.interface.increaseMotorAngleReferences(wheels, circular_distances)
 
-			motorAngles_end.append(round(motorAngles_start[0][0] + circular_distances[0],2))
-			motorAngles_end.append(round(motorAngles_start[1][0] + circular_distances[1],2))
-			print("Angles to end at: {}".format(motorAngles_end))
+		# This function does PID control until angle references are reached
+		while not self.interface.motorAngleReferencesReached(wheels):
+			#time.sleep(0.1)
+			#print(self.interface.getMotorAngles(wheels))
+			if (round(self.interface.getMotorAngles(wheels)[0][0],2)==motorAngles_end[0] or round(self.interface.getMotorAngles(wheels)[1][0],2)==motorAngles_end[1]):
+				return True
+		return True
 
-			self.interface.increaseMotorAngleReferences(wheels, circular_distances)
-
-			# This function does PID control until angle references are reached
-			while not self.interface.motorAngleReferencesReached(wheels):
-				time.sleep(0.1)
-				print(self.interface.getMotorAngles(wheels))
-				if (round(self.interface.getMotorAngles(wheels)[0][0],2)==motorAngles_end[0] or round(self.interface.getMotorAngles(wheels)[1][0],2)==motorAngles_end[1]):
-					return True
-			return True
-		else:
-			#velocity = k(distance-measured distance)
-
+	# Rotate a motor by angle degrees (mainly for ultrasound motor)
+	def rotate_motor(self, angle, motors=[2]):
+		self.interface.increaseMotorAngleReferences(motors, angle*self.ultra_angle_calibration)
+		# This function does PID control until angle references are reached
+		while not self.interface.motorAngleReferencesReached(wheels):
+			pass
+		return True
 
 	#Takes the distance in centimeters and moves it forward
 	def travel_straight(self, distance):
@@ -123,11 +145,13 @@ class Robot:
 	#Takes the angle in degrees and rotates the robot right
 	def rotate_right(self, angle):
 		dist = self.angle_calibration*angle
+		self.pose = self.pose + angle
 		return self.move_wheels([-dist,dist])
 
 	#Takes the angle in degrees and rotates the robot left
 	def rotate_left(self, angle):
-		self.rotate_right(-angle)
+		self.pose = self.pose - angle
+		return self.rotate_right(-angle)
 
 	#Does the immediate stop if it runs into an obstacle
 	def stop(self):
