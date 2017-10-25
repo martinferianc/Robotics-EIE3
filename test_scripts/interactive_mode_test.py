311a4963acca1d00d581b7b965b5dc86f58e6763
FCPH Pi <pi@brickpi>
2017-10-25 11:56:18 +0000

    Added test file for interactive mode

---
+++ test_scripts/interactive_mode_test.py
@@ -0,0 +1,18 @@
+import threading
+import time
+import sys
+from src.robot import Robot
+import brickpi
+
+#Initialize the interface
+interface=brickpi.Interface()
+interface.initialize()
+interface.startLogging("motor_position_1.log")
+
+Robot = Robot(interface,'paper_config.json')
+
+
+Robot.interactive_mode()
+
+interface.stopLogging()
+interface.terminate()
