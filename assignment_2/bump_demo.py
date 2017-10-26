0391da3fb55523f2a3c871fe058fe7e94cc1b982
FCPH Pi <pi@brickpi>
2017-10-26 11:38:38 +0000

    [Assignment 2] Updated bump

--- assignment_2/bump_demo.py
+++ assignment_2/bump_demo.py
@@ -13,7 +13,9 @@ right_bumper_port = 0
 config = str(sys.argv[1])
 Robot = Robot(interface, pid_config_file=config)
 bumpers = [None,None]
-Robot.set_speed([2,2])
+speeds = [5,5]
+zeros = [0,0]
+Robot.set_speed(speeds)
 Robot.start_threading()
 
 while True:
@@ -22,15 +24,21 @@ while True:
 
     if bumpers[0] and bumpers[1]:
         Robot.stop()
+	Robot.set_speed(zeros)
         Robot.travel_straight(-3)
         Robot.rotate_right(30)
+	Robot.set_speed(speeds)
 
     if not bumpers[0] and bumpers[1]:
         Robot.stop()
+	Robot.set_speed(zeros)
         Robot.rotate_left(30)
-
+	Robot.set_speed(speeds)
     if bumpers[0] and not bumpers[1]:
         Robot.stop()
+	Robot.set_speed(zeros)
         Robot.rotate_right(30)
+	Robot.set_speed(speeds)
+	
 
 interface.terminate()
