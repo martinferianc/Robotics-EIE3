3886ea7ef777088dfe67db9ea2b11dac13f2966e
FCPH Pi <pi@brickpi>
2017-10-26 12:04:22 +0000

    [Assignment 2] Bump demo with thread

--- assignment_2/bump_demo.py
+++ assignment_2/bump_demo.py
@@ -8,40 +8,50 @@ import sys
 interface=brickpi.Interface()
 interface.initialize()
 
-left_bumper_port = 2
-right_bumper_port = 0
-config = str(sys.argv[1])
-Robot = Robot(interface, pid_config_file=config)
-bumpers = [None,None]
-speeds = [5,5]
-zeros = [0,0]
-Robot.set_speed(speeds)
-Robot.start_threading()
+def bump():
+    while True:
+        bumpers[0] = Robot.get_bumper("left")
+        bumpers[1] = Robot.get_bumper("right")
 
-while True:
-    bumpers[0] = Robot.get_bumper("left")
-    bumpers[1] = Robot.get_bumper("right")
+        #print(bumpers)
+        #print(Robot.get_distance())
 
-    if bumpers[0] and bumpers[1]:
-        Robot.stop()
-        time.sleep(1)
-        Robot.set_speed(zeros)
-        Robot.travel_straight(-3)
-        Robot.rotate_right(30)
-        Robot.set_speed(speeds)
+        if bumpers[0] and bumpers[1]:
+            Robot.stop()
+            #time.sleep(1)
+            Robot.set_speed(zeros)
+            Robot.travel_straight(-3)
+            Robot.rotate_right(30)
+            Robot.set_speed(speeds)
 
-    if not bumpers[0] and bumpers[1]:
-        Robot.stop()
-        time.sleep(1)
-        Robot.set_speed(zeros)
-        Robot.rotate_left(30)
-        Robot.set_speed(speeds)
-    if bumpers[0] and not bumpers[1]:
-        Robot.stop()
-        time.sleep(1)
-        Robot.set_speed(zeros)
-        Robot.rotate_right(30)
-        Robot.set_speed(speeds)
+        if not bumpers[0] and bumpers[1]:
+            Robot.stop()
+            #time.sleep(1)
+            Robot.set_speed(zeros)
+            Robot.rotate_left(30)
+            Robot.set_speed(speeds)
+        if bumpers[0] and not bumpers[1]:
+            Robot.stop()
+            #time.sleep(1)
+            Robot.set_speed(zeros)
+            Robot.rotate_right(30)
+            Robot.set_speed(speeds)
+
+if __name__=="__main__":
+
+    config = str(sys.argv[1])
+    Robot = Robot(interface, pid_config_file=config)
+    bumpers = [None,None]
+    speeds = [5,5]
+    zeros = [0,0]
+    Robot.set_speed(speeds)
+    Robot.start_threading() 
 
 
-interface.terminate()
+    threads = []
+    t = threading.Thread(target=bump)
+    threads.append(t)
+    t.start()
+    interface.terminate()
+
+
