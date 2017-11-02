76e8fd627c8bdec5b34cc4e63661a64e46bc9ccb
Owen Harcombe <owen.harcombe@gmail.com>
2017-11-02 17:34:46 +0000

    Inverted angle for consistency

--- src/robot.py
+++ src/robot.py
@@ -261,9 +261,8 @@ class Robot:
 		diff_Y = (Y*100)-current_y
 		distance = math.sqrt(math.pow(diff_X,2)+math.pow(diff_Y,2))
 
-		angle = math.atan2(diff_Y,diff_X)
-		self.set_robot_pose(-angle)
-
+		angle = math.atan2(-diff_Y,diff_X)
+		self.set_robot_pose(angle)
 		return self.travel_straight(distance,update_particles=True)
 
 
