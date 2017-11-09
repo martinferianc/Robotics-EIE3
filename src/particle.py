656ac8c4760a2029effae8d9885a6135db2dce47
George Punter <gsp14@matrix44.doc.ic.ac.uk>
2017-11-09 11:15:33 +0000

    [Assignment 3] hotfix

--- src/particle.py
+++ src/particle.py
@@ -14,14 +14,14 @@ class ParticleState():
             for point in self.state:
                 e_x=random.gauss(0,self.standard_deviation["x"])
                 e_y=random.gauss(0,self.standard_deviation["y"])
-                e_theta=random.gauss(0,self.standard_deviation["theta"])
+                e_theta=random.gauss(0,self.standard_deviation["theta_straight"])
                 point[0][0]+=(movement + e_x)*math.cos(point[0][2])
                 point[0][1]+=(movement + e_y)*math.sin(point[0][2])
                 point[0][2]+=e_theta
         elif action == "rotation":
             # movement is the amount of rotation
             for point in self.state:
-		point[0][2] += math.radians(movement) + random.gauss(0,self.standard_deviation["theta"])
+		point[0][2] += math.radians(movement) + random.gauss(0,self.standard_deviation["theta_rotate"])
         else:
             raise Exception("Not a valid action!")
         return True
@@ -29,7 +29,7 @@ class ParticleState():
 	mean_y = np.mean(np.array([point[0][1] for point in self.state]))
         mean_x = np.mean(np.array([point[0][0] for point in self.state]))
         mean_theta = np.mean(np.array([point[0][2] for point in self.state]))
-        return (mean_x, mean_y, mean_theta)    
+        return (mean_x, mean_y, mean_theta)
     def reset(self):
 	self.state = [([0,0,0],1/self.number_of_particles) for x in xrange(self.number_of_particles)]
     def get_state(self):
