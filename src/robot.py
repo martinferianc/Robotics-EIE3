69957c3bf5cbb949d427ea1c133a8bc5d6ac82a0
Martin Ferianc <ferianc.martin@gmail.com>
2017-11-14 15:01:55 +0000

    Fixing ultrasound

--- src/robot.py
+++ src/robot.py
@@ -368,10 +368,10 @@ class Robot:
 			wall_distance = {}
 			self.set_ultra_pose(90)
 			time.sleep(2)
-			wall_distance['90'] = self.get_distance
+			wall_distance['90'] = self.get_distance()
 			self.set_ultra_pose(-90)
 			time.sleep(2)
-			wall_distance['-90'] = self.get_distance
+			wall_distance['-90'] = self.get_distance()
 			self.set_ultra_pose(0)
 			print "Ultrasound: {0}".format(min(wall_distance))
 			print "Ultrasound: {0}".format(min(wall_distance, key=wall_distance.get))
